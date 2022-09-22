8e41e7822ad40c11ea74f0b38d067bb809c2358e
Unmesh28 <unmesh.dabhade@gmail.com>
2022-09-22 16:33:07 +0530

    first commit

--- collision_warning.py
+++ collision_warning.py
@@ -28,28 +28,28 @@ from pynput.keyboard import Key, Controller
 
 def func():
     # Define and parse input arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
-                        default='./India_model/')
-    parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
-                        default='detect.tflite')
-    parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
-                        default='labelmap.txt')
-    parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
-                        default=0.5)
-    parser.add_argument('--video', help='Name of the video file',
-                        default='00380017.AVI')
-    parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
-                        action='store_true')
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
+    #                     default='./India_model/')
+    # parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
+    #                     default='detect.tflite')
+    # parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
+    #                     default='labelmap.txt')
+    # parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
+    #                     default=0.5)
+    # parser.add_argument('--video', help='Name of the video file',
+    #                     default='00380017.AVI')
+    # parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
+    #                     action='store_true')
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    MODEL_NAME = args.modeldir
-    GRAPH_NAME = args.graph
-    LABELMAP_NAME = args.labels
-    VIDEO_NAME = args.video
-    min_conf_threshold = float(args.threshold)
-    use_TPU = args.edgetpu
+    MODEL_NAME = './India_model/'
+    GRAPH_NAME = 'detect.tflite'
+    LABELMAP_NAME = 'labelmap.txt'
+    VIDEO_NAME = '00380017.AVI'
+    min_conf_threshold = float(0.5)
+    use_TPU = 'store_true'
 
     keyboard = Controller()
 
